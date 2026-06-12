856aeafa2d7242bd2431188193ff535541383bf8
l-lacker <37620897+l-lacker@users.noreply.github.com>
2026-06-12 18:57:26 +0300

    оставлены только основные иконки lawxgram
    в настройках выбора иконки теперь есть только основная и тёмная иконки lawxgram, а тёмная использует отдельный фон #141414.
    
    удалены старые альтернативные иконки и связанные строки, оставшиеся от telegram/nekogram, чтобы в проекте не было мёртвых launcher-ресурсов.
    
    у основной иконки снят monochrome-слой и ночной override фона, поэтому она больше не становится themed icon от темы телефона. splash оставлен с отдельными light/dark цветами, но иконка на нём теперь рисуется круглой.

--- TMessagesProj/src/main/java/org/telegram/ui/ThemeActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ThemeActivity.java
@@ -2586,7 +2586,7 @@ public class ThemeActivity extends BaseFragment implements NotificationCenter.No
                     } else if (position == selectThemeHeaderRow) {
                         headerCell.setText(getString("SelectTheme", R.string.SelectTheme));
                     } else if (position == appIconHeaderRow) {
-                        headerCell.setText(String.format("%s %s", getString(R.string.AppIcon), "@RKBDI"));
+                        headerCell.setText(getString(R.string.AppIcon));
                     } else if (position == otherHeaderRow) {
                         headerCell.setText(getString("OtherSettings", R.string.OtherSettings));
                     } else if (position == mediaSoundHeaderRow) {
